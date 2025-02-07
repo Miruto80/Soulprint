c1c91864f3d2aeea8a97531705e8af1ef0b04495
Miruto80 <miguelfernando8012@gmail.com>
2025-02-07 13:14:15 -0400

    first commit

---
+++ js/index.js
@@ -0,0 +1,137 @@
+function openForm(car) {
+    document.getElementById('car').value = car;
+    document.getElementById('contactForm').style.display = 'block';
+}
+
+function closeForm() {
+    document.getElementById('contactForm').style.display = 'none';
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+const carDivs = document.querySelectorAll('.car');
+
+carDivs.forEach(carDiv => {
+const images = carDiv.querySelectorAll('.car-images img');
+images.forEach((img, index) => {
+    if (index === 0) {
+        img.style.display = 'block'; // Muestra solo la primera imagen
+    } else {
+        img.style.display = 'none'; // Oculta todas las demás imágenes
+    }
+});
+});
+});
+
+function prevImage(button) {
+const carDiv = button.closest('.car');
+const images = carDiv.querySelectorAll('.car-images img');
+let currentIndex = Array.from(images).findIndex(img => img.style.display === 'block');
+
+images[currentIndex].style.display = 'none';
+if (currentIndex === 0) {
+images[images.length - 1].style.display = 'block';
+} else {
+images[currentIndex - 1].style.display = 'block';
+}
+}
+
+function nextImage(button) {
+const carDiv = button.closest('.car');
+const images = carDiv.querySelectorAll('.car-images img');
+let currentIndex = Array.from(images).findIndex(img => img.style.display === 'block');
+
+images[currentIndex].style.display = 'none';
+if (currentIndex === images.length - 1) {
+images[0].style.display = 'block';
+} else {
+images[currentIndex + 1].style.display = 'block';
+}
+}
+
+// Validaciones para los campos de nombre, teléfono y email
+$("#contact").on("keypress", function (e) {
+    validarkeypress(/^[A-Za-z@_.0-9\b\u00f1\u00d1\u00E0-\u00FC-]*$/, e);
+});
+
+$("#contact").on("keyup", function () {
+    let valor = $(this).val();
+    let emailRegex = /^[A-Za-z_0-9\u00f1\u00d1\u00E0-\u00FC-]{3,20}[@]{1}[A-Za-z0-9]{3,8}[.]{1}[A-Za-z]{3}$/;
+    let phoneRegex = /^[0-9]{10,11}$/;
+    
+    if (emailRegex.test(valor) || phoneRegex.test(valor)) {
+        $("#scontact").text("");
+        return 1;
+    } else {
+        $("#scontact").text("Enter a valid email or phone");
+        return 0;
+    }
+});
+
+$("#name").on("keypress", function (e) {
+validarkeypress(/^[A-Za-z\b\s\u00f1\u00d1\u00E0-\u00FC]*$/, e);
+});
+
+$("#name").on("keyup", function () {
+validarkeyup(/^[A-Za-z\b\s\u00f1\u00d1\u00E0-\u00FC]{3,30}$/, $(this), $("#sname"), "Only letters between 3 and 30 characters");
+});
+
+// Función de validación del envío
+function validarenvio() {
+    if ($("#contact").val().length === 0 || validarkeyup(/^[A-Za-z_0-9\u00f1\u00d1\u00E0-\u00FC-]{3,20}[@]{1}[A-Za-z0-9]{3,8}[.]{1}[A-Za-z]{3}$/, 
+    $("#contact"), $("#scontact"), "Enter a valid email or phone") == 0 && 
+    validarkeyup(/^[0-9]{10,11}$/, $("#contact"), $("#scontact"), "Enter a valid email or phone") == 0) {
+    alert("Error in email or phone");
+    return false;
+}
+
+if (validarkeyup(/^[A-Za-z\b\s\u00f1\u00d1\u00E0-\u00FC]{3,30}$/, $("#name"), $("#sname"), "Only letters between 3 and 30 characters") == 0) {
+alert("Error in name");
+return false;
+}
+return true;
+}
+
+// Función para validar la entrada mientras se escribe
+function validarkeypress(er, e) {
+key = e.keyCode;
+tecla = String.fromCharCode(key);
+a = er.test(tecla);
+if (!a) {
+e.preventDefault();
+}
+}
+
+// Función para validar la entrada después de escribir
+function validarkeyup(er, etiqueta, etiquetamensaje, mensaje) {
+a = er.test(etiqueta.val());
+if (a) {
+etiquetamensaje.text("");
+return 1;
+} else {
+etiquetamensaje.text(mensaje);
+return 0;
+}
+}
+
+let currentImageIndex = 0;
+const images = [
+    "imagenes/Sueters pareja.jpeg", 
+    "imagenes/Banner.jpeg"
+];
+const carouselBg = document.getElementById("carousel-bg");
+
+function nexttImage() {
+    currentImageIndex = (currentImageIndex + 1) % images.length;
+    carouselBg.src = images[currentImageIndex];
+}
+
+function previousImage() {
+    currentImageIndex = (currentImageIndex - 1 + images.length) % images.length;
+    carouselBg.src = images[currentImageIndex];
+}
+
+
+document.getElementById('legal-button').addEventListener('click', function () {
+    const legalContent = document.getElementById('legal-content');
+    legalContent.classList.toggle('show');
+});
